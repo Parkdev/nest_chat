d5102f5fc9ecdfbe08e59467a7ec810c1cc1da72
Parkdev <Parkdev@github.com>
2024-04-29 09:46:59 +0900

    0429 add chat logic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,18 +6,59 @@ const getElementById = (id) => document.getElementById(id) || null;
 //* get DOM element
 const helloStrangerElement = getElementById('hello_stranger');
 const chattingBoxElement = getElementById('chatting_box');
-const formElemnt = getElementById('chat_form');
+const formElement = getElementById('chat_form'); // 뷰에 인댁스에 chatform
+
+//* global socket handler
+socket.on('user_connected', (username) => {
+  drawNewChat(`${username} connected`);
+});
+
+socket.on('new_chat', (data) => {
+  const { chat, username } = data;
+  drawNewChat(`${username} : ${chat}`);
+});
+
+//* event callback functions
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const inputValue = event.target.elements[0].value;
+  console.log(inputValue);
+  if (inputValue !== '') {
+    socket.emit('send_message', inputValue);
+    //화면에 그리기
+
+    drawNewChat(`me : ${inputValue}`);
+    event.target.elements[0].value = '';
+  }
+};
+
+//* draw functions
+const drawHelloStranger = (username) => {
+  return (helloStrangerElement.innerText = `Hello ${username} Stranger :)`);
+};
+
+const drawNewChat = (message) => {
+  const wrapperChatBox = document.createElement('div');
+  const chatBox = `
+  <div>
+  ${message}
+  </div>
+  `;
+  wrapperChatBox.innerHTML = chatBox;
+  chattingBoxElement.appendChild(wrapperChatBox);
+};
 
 function helloUser() {
   const username = prompt('What is your name?');
-  socket.emit('new_user', username);
-  socket.on('welcome', (data) => {
-    console.log(data);
+  socket.emit('new_user', username, (data) => {
+    drawHelloStranger(data);
   });
 }
 
 function init() {
   helloUser(); // 접속했을때 확인창 뜨는 함수
+  // 이벤트 연결
+  formElement.addEventListener('submit', handleSubmit);
 }
 
 init();
--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -20,33 +20,56 @@ export class ChatsGateway
     this.logger.log('constructor');
   }
 
-  handleDisconnect(@ConnectedSocket() socket: Socket) {
-    console.log('클라이언트 접속 해제');
-    this.logger.log(`connect : ${socket.id} ${socket.nsp.name}`);
-  }
-
   //handleConnection(client: any, ...args: any[]) { // 연결이 되는순간 실행
   // console.log('클라이언트 접속', client.id);
   //}
-
-  handleConnection(@ConnectedSocket() socket: Socket) {
-    console.log('클라이언트 접속');
-    this.logger.log(`connect : ${socket.id} ${socket.nsp.name}`);
-  }
 
   afterInit() {
     console.log('소켓 서버가 초기화되었습니다.');
     this.logger.log('init');
   }
 
-  @SubscribeMessage('new_user') // new_user라는 이벤트를 받았을 때
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    console.log('클라이언트 접속');
+    this.logger.log(`connect : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    console.log('클라이언트 접속 해제');
+    this.logger.log(`connect : ${socket.id} ${socket.nsp.name}`);
+  }
+
+  //   @SubscribeMessage('new_user') // new_user라는 이벤트를 받았을 때
+  //   handleNewUser(
+  //     @MessageBody() username: string, // 바디로부터 username을 받아온다.
+  //     @ConnectedSocket() socket: Socket, // 이 소캣으로 emit|on 할 수 있다.
+  //   ): string {
+  //     console.log('소캣id', socket.id); //소캣id zCSBcBveGDK8vRvcAAAB
+  //     console.log('유저', username); // 유저 홍길동
+  //     // socket.emit('welcome', `환영합니다 ${username}님!`); // welcome 이벤트를 발생시킨다.
+  //     return username;
+  //   }
+  // }
+
+  @SubscribeMessage('new_user')
   handleNewUser(
-    @MessageBody() username: string, // 바디로부터 username을 받아온다.
-    @ConnectedSocket() socket: Socket, // 이 소캣으로 emit|on 할 수 있다.
-  ): string {
-    console.log('소캣id', socket.id); //소캣id zCSBcBveGDK8vRvcAAAB
-    console.log('유저', username); // 유저 ㅁㄴㅇㄹ
-    socket.emit('welcome', `환영합니다 ${username}님!`); // welcome 이벤트를 발생시킨다.
-    return 'Hello world!';
+    @MessageBody() username: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    // username db에 적재
+    // 이후 브로드캐스팅 적제
+    socket.broadcast.emit('user_connected', username); // 연결된 모든 소캣에 데이터를 보냄
+    return username;
+  }
+
+  @SubscribeMessage('send_message')
+  handleSubmitChat(
+    @MessageBody() chat: string,
+    @ConnectedSocket() socket: Socket,
+  ) {
+    socket.broadcast.emit('new_chat', {
+      chat,
+      username: socket.id,
+    }); // 연결된 모든 소캣에 데이터를 보냄
   }
 }
